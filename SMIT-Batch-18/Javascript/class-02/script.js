7dcb09c59baf4cb85625dd8d7a56bc0c6f027833
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-01-11 21:10:12 +0500

    class 02 assignment

---
+++ SMIT-Batch-18/Javascript/class-02/script.js
@@ -0,0 +1,76 @@
+// ============ Chapter 1 ================
+
+// Question No 1
+// alert("hello");
+
+// Question No 2
+// alert("error please enter a valid passsword");
+
+// Question 3
+// alert("welcome to js land \thappy coding")
+
+// Question No 4
+// Solved in html file
+
+// var fullname ="palwasha qamar"
+// alert(fullname)
+// var age ="15 year"
+// var bio ="I am student in SMIT"
+// alert(bio)
+
+// age ="25"
+// alert(age)
+
+// var product = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// alert(product)
+
+// var email = "zoha112@gmail.com"
+// alert("My email name is " + email)
+
+// var book ="A smarter way to learn javascript"
+// alert(book)
+
+// document.write("<h1>yah! i can write html content through javascript</h1>")
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// var age =18
+// alert(age)
+
+// var birthyear =2007
+// alert(birthyear)
+
+// document.write("my birth year is 2007 <br> the data type in alert box is just a number " )
+
+// var username ="John Doe"
+
+// var productTitle="T-shirt"
+
+// var productQuantity= 5
+
+// alert(username + " " + "order " + productQuantity + " " + productTitle + " (S)on XYZ clothing store")
+
+// Question 1
+// var num1 = 10
+// var num2 = 20
+// var sum = num1 + num2
+// alert(sum)
+
+// Question 2
+// var num1 = 20
+// var num2 = 10
+// var sum = num1 % num2
+// alert(sum)
+
+// Question 3
+// var num;
+// document.writeln("Value after variable declaration is: " + num )
+// num = 5
+// document.write("Initial value: " + num + "<br>")
+// num = num + 1
+// document.write("Value after increment is: " + num + "<br>")
+// num = num + 7
+// document.write(num)
+// num = num - 1
+// document.write("Value after decrement is: " + num + "<br>")
+// num = num % 3
+// document.write(num)
