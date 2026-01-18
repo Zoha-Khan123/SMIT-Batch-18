1c112654163d91a5c3cd671ccdd162a2ec27b74d
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-01-19 01:30:24 +0500

    javascript class 03

---
+++ SMIT-Batch-18/Javascript/class-03/script.js
@@ -0,0 +1,104 @@
+// ================ Chapter 6 ==============
+// var num = 5
+// // num = num + 1
+// num++
+// num++
+// console.log(num)
+
+// // Post increment
+// var num = 5
+// var newNum = num++
+// console.log(newNum);
+
+// // pre increment
+// var num = 5
+// var newNum = ++num
+// console.log(newNum);
+
+// Post decrement
+// var num = 5
+// var newNum = num--
+// console.log(newNum);
+
+// pre decrement
+// var num = 5
+// var newNum = --num
+// console.log(newNum);
+
+// ============= Practice Questions of Increment and Decrement =======
+// var a = 5
+// var b = 6
+// var c = a++ - b-- - --b + --a
+// //      5  -   6   - 4  + 5
+// console.log(c);
+
+// let i = 2;
+// let result = i++ + ++i;
+// //            2   + 4
+// console.log(result);
+
+// let z = 5;
+// z = z++ - --z;
+// //   5  - 5
+// console.log(z);
+
+// var a = 4;
+// var b = ++a;   //5
+// b = b + a++;
+// //   5 + 5
+// console.log(b);
+// console.log(a);
+
+// ================ Chapter 7 ==============
+
+// var totalCost = (1 + 3) * 4;
+// console.log(totalCost);
+
+// var resultOfComputation = ((2 * 4) * 4) + 2;
+// console.log(resultOfComputation);
+
+// ================== Chapter 8 =============
+// alert("2" + 2)
+// alert("2" + "2")
+
+// alert("2 plus 2 equals " + 2 + 2);
+
+// =============== Practice question ============
+
+// Question 01
+// console.log("5" + 3 - 2);
+// //  "53" - 2
+// // 53 - 2
+// // 51
+
+// Question 02
+// console.log("10" - "4" - 3 + "2");
+// //              10 - 4 = 6
+// //              6 - 3 =  3
+// //              3 + "2" = "32"
+
+// ================= chapter 9 ==========
+// var storeData = prompt("Enter your name", "Zoha")
+// console.log(typeof storeData);
+// console.log(2 - storeData);
+
+// var num = prompt("Enter a number")
+// console.log(num + " X 1 = " + num*1);
+// console.log(num + " X 2 = " + num*2);
+// console.log(num + " X 3 = " + num*3);
+// console.log(num + " X 4 = " + num*4);
+// console.log(num + " X 5 = " + num*5);
+// console.log(num + " X 6 = " + num*6);
+// console.log(num + " X 7 = " + num*7);
+// console.log(num + " X 8 = " + num*8);
+// console.log(num + " X 9 = " + num*9);
+// console.log(num + " X 10 = " + num*10);
+
+// var celsius = 25
+// var fare = (celsius * 9 / 5)  + 32
+// console.log(celsius + "°C is " + fare + "°F");
+
+// var currentYear = prompt("ENTER CURRENT YEAR")
+// var birthYear = prompt("ENTER BIRTHyEAR YEAR")
+// var calculateAge = currentYear - birthYear
+// console.log(calculateAge);
