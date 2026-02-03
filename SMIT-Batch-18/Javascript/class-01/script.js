090d5d74a0929fddddcbb034f754983ef4453bef
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-02-04 00:38:01 +0500

    class-04-work

---
+++ SMIT-Batch-18/Javascript/class-01/script.js
@@ -0,0 +1,43 @@
+// alert(true)
+// window.alert("hello")
+
+// ========== Variables for string ==========
+// var myFullName = "Zoha Khan"
+// alert(myFullName)
+// myFullName = "Ayesha"
+// alert(myFullName)
+
+
+// var fullName;      //declaration
+// fullName = "ZOHA"  //assign 
+
+
+
+//=================== Variables fro Numbers =======
+// var weight = 150;
+// alert(weight)
+// weight = 150 + 10
+// alert(weight)
+
+// var num1 = 10
+// var num2 = 20
+// alert(num1 + num2)
+
+// var originalNumber = "10"
+// var newNumber = originalNumber + 20
+// alert(newNumber)
+
+
+// var 1stclass = "Javac"  //no
+// var $a_1bc
+// var a = "hello"
+// alert(A)
+// var a = "world"
+
+
+// var myfullname
+
+
+
+// ================ Assignmet 1 =========
+// alert("Hello World")
