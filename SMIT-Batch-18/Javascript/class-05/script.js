ac7ddc5c29f526b35f126c236aa17ac8037b4e07
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-02-07 13:12:31 +0500

    class 05

---
+++ SMIT-Batch-18/Javascript/class-05/script.js
@@ -0,0 +1,61 @@
+// alert("Hello")
+// alert()
+
+
+// var myname = "Ayesha";
+// myname = "Zoha";
+// console.log(myname);
+
+
+// var name1 = "one"
+// var my name = "Zoha"
+// var myFullName
+// var my_full_name
+// var number = 1;
+
+// var myVar
+// var rose = "Jasmine"
+// console.log(rose);
+// var Rose = "Sunflower"
+
+// var name = "Zoha"
+// var num1 = 9
+// var num2 = 3
+// console.log(num1 % num2); 
+
+// var num = 2
+// var num1 = --num
+
+// console.log(num1);
+
+// var totalCost = (1 + 3) * 4
+
+
+// var firstName = "Zoha"
+// var lastName = "Khan"
+// console.log("Hello! " + firstName + lastName);
+
+// var num1 = 2
+// var num2 = 4
+// console.log("Sum of num1 and num2 " + (num1 + num2));
+
+// var num1 = Number(prompt("Enter fisrt number"))
+// var num2 = +prompt("Enter second number")
+// console.log(num1 + num2)
+
+
+// var myName = prompt("Enter your name")
+// console.log(myName);
+
+
+
+var obtainedMakrs = prompt("Enter your gender")
+totalMakrs = 100
+percentage = obtainedMakrs * 100 /100
+
+if(gender === "female"){
+    console.log("you are allowed");
+}
+
+
+    
