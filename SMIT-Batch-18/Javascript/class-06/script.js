5a2838d55eb79ee225a1894663fa3166647ae345
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-02-16 21:26:53 +0500

    class 06

---
+++ SMIT-Batch-18/Javascript/class-06/script.js
@@ -0,0 +1,128 @@
+// var age = prompt("Enter your age")
+
+// if(age > 18){
+//     console.log("You can vote");
+// }
+
+
+// var number = prompt("Enter any number")
+
+// if(number % 2 === 0){
+//     console.log("Even number");
+// }
+
+
+// var userPassword = prompt("Enter your password");
+// var databasePassword = "12345";
+
+// if(userPassword === databasePassword){
+//     console.log("Login successful");
+// }
+
+
+// var city = prompt("Enter your city")
+// if(city !==  "Karachi"){
+//     console.log("Ap karachi say belong nahi kartay"); 
+// }
+
+
+
+// if("12" !== 10){
+//     console.log("pass"); 
+// }
+
+
+
+// var username = prompt("Enter your name")
+
+// if(username === "Zoha"){
+//     console.log("Welcome Zoha");
+// }else{
+//     console.log("Invalid username");
+// }
+
+
+// var dress = prompt("Enter color")
+
+// if(dress === "red"){
+//     console.log("Buy this red color");
+// }else if(dress === "blue"){
+//     console.log("Buy this blue color");
+// }else if(dress === "yellow"){
+//     console.log("Buy this yellow color");
+// }else{
+//     console.log("Jo bhe milay lay ana");
+// }
+
+
+
+// var trafficLights = prompt("Enter traffic color")
+
+// if(trafficLights === "red"){
+//     console.log("Stop");
+// }else if(trafficLights === "yellow"){
+//     console.log("Get ready");
+// }else if(trafficLights === "green"){
+//     console.log("Go");
+// }else{
+//     console.log("Invalid Color");
+// }
+
+
+// var ticketFees = +prompt("Enter amount");
+// var permission = prompt("Parents permission (yes / no)")
+
+
+// if(ticketFees === 1000 && permission === "yes"){
+//     console.log("Ab tum picinic pay ja saktay ho");
+// }else{
+//     console.log("tum nahi ja saktay");
+    
+// }
+
+
+// var day = prompt("Enter day")
+
+// if(day === "Saturday" || day === "Sunday"){
+//     console.log("Mazy karo");
+// }else{
+//     console.log("School jaw");
+// }
+
+
+// if(!0){
+//     console.log("True ha");
+// }
+
+
+// Comparison operator
+// ===
+// !==
+// >
+// <
+// >=
+// <=
+
+
+// &&
+// true  && true = true
+// true && false = false
+// false && true = false
+// false && false = false
+
+// ||
+// true  || true = true
+// true || false = true
+// false || true = true
+// false || false = false
+
+
+// Not !
+// !true   -> false
+// !false  -> true
+// !1      -> false
+// !0      -> true
+
+
+// True -> 1
+// False -> 0
