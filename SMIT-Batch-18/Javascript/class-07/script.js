731498feaa6f8b4d06e1ee60636a9b1e8465dfea
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-02-18 01:14:21 +0500

    class 07 code

---
+++ SMIT-Batch-18/Javascript/class-07/script.js
@@ -0,0 +1,81 @@
+// var num = prompt("Enter any number")
+
+// if (num < 0){
+//     console.log("Number is negative");
+// }else if (num < 50) {
+//     console.log("Number is less than 50");
+// }else {
+//     console.log("number is greater than 50");
+// }
+
+// var username = prompt("Enter your name")
+
+// // if(username.toLowerCase() === "zoha"){
+// //     console.log("Welcome Zoha");
+
+// // }
+
+// &&
+// ||
+// !
+
+// var num = +prompt("Enter any number")
+
+// if(num > 0 && num <= 50){
+//     console.log("Number is between 1-50");
+
+// }else if(num >= 51 && num <= 100){
+//     console.log("Number is between 51-100");
+
+// }else{
+//     console.log("Number is not in between 1-100");
+
+// }
+
+// var num = +prompt("Enter any number")
+
+// if(num >= 1 && num <= 10 || num >= 91 && num <= 100){
+//     console.log("Number is between 1-10 or 91-100");
+
+// }else if(num >= 11 && num <= 90){
+//     console.log("Number is between 11-90");
+
+// }else{
+//     console.log("Number is not in between 1-100");
+
+// }
+
+// 2
+
+// (num >= 1 && num <= 10) || (num >= 91 && num <= 100)
+//     true  &&  true      ||  false    &&  true
+//         true            ||        false
+//                        true
+
+// var num = +prompt("Enter any number")
+
+// if(num >= 1 && num <= 10 || num >= 91 && num <= 100){
+
+//     if(num >= 1 && num <= 10){
+//         console.log("Number is between 1-10");
+//     }else{
+//         console.log("Number is between 91-100");
+//     }
+
+// }else if(num >= 11 && num <= 90){
+//     console.log("Number is between 11-90");
+
+// }else{
+//     console.log("Number is not in between 1-100");
+
+// }
+
+// var ticket = +prompt("Enter your ticket amount");
+// var parentsPermission = prompt("Parenst permission (yes/no)")
+
+// if(ticket === 1000 || parentsPermission === "yes"){
+//     console.log("Theek ha hum la jaingay");
+// }else{
+//     console.log("Hum nahi la kay ja rahay");
+
+// }
