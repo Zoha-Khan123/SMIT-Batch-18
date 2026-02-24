061955d645abb9a6f1b6cb6f2ba95fb9773d548a
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-02-24 13:13:53 +0500

    class 09 code

---
+++ SMIT-Batch-18/Javascript/class-09/script.js
@@ -0,0 +1,57 @@
+// var cities = ["Karachi","Islamabad","Quetta","Lahore"]
+// console.log(cities[2]);
+
+// cities[1] = "Peshawar"
+
+// cities[cities.length - 1] = "Multan"
+// console.log(cities);
+
+
+// var cities = ["Karachi","Islamabad","Quetta","Lahore"]
+// console.log("before method " , cities);
+
+// // slice
+// var copyValue = cities.slice(-2)
+// console.log("after method " , copyValue);
+
+
+// // splice
+// var returnValue = cities.splice(1,2,"Multan","Peshawar")
+// console.log(returnValue);
+
+
+// shift
+// var returnValue = cities.shift()
+// console.log("Return Value" , returnValue);
+// console.log("after method" , cities);
+
+// unshift
+// var returnValue = cities.unshift("Multan","Peshawar","Lahore")
+// console.log("Return Value" , returnValue);
+// console.log("after method" , cities);
+
+
+// pop
+// var returnValue = cities.pop()
+// console.log("Return Value" , returnValue)
+// console.log("after method", cities);
+
+
+// push
+// var retrunValue = cities.push("Multan","Peshawar")
+// console.log(retrunValue);
+// console.log(cities);
+
+
+// ========= for loop ===========
+// for(var i = 0; i <= 20; i++){
+// console.log(i + " Hello World");
+// }
+
+
+
+// i ke value        condition        code     increment
+// 1                  1 <= 10           1        1 + 1  = 2
+// 2                  2 <= 10           2        2 + 1 = 3
+// 10                 10 <= 10          10       10 + 1 = 11
+// 11                 11 <= 10
