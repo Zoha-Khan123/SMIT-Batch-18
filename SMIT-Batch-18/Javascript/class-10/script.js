657143069a64342f03d18abfd0236f5d3ed5afa2
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-02-28 13:23:35 +0500

    class 10 code

---
+++ SMIT-Batch-18/Javascript/class-10/script.js
@@ -0,0 +1,73 @@
+
+// ============ array with loop ============
+// var cities = ["Karachi","Islamabad","Multan","Quetta","Lahore","Peshawar","Hyderabad"]
+// for(var i = 0; i < cities.length; i++){
+// console.log(cities[i]);
+// }
+
+// ======= simple table with for loop =========
+// 2 x 1 = 2
+// 2 x 2 = 4
+// 2 x 3 = 6
+
+// var table = prompt("Enter any number");
+// var tableLength = prompt("Enter table length")
+// for(var i = 1; i <= tableLength; i++){
+//     console.log(table*i);
+// }
+
+// ======= complete table =========
+// var table = prompt("Enter any number");
+// for(var i = 1; i <= 10; i++){
+//     console.log(table + " X " + i + " = " + table*i);
+// }
+
+
+// ================== flag variable and boolean values =============
+// var cities = ["Karachi","Islamabad","Multan","Quetta","Lahore","Peshawar","Hyderabad"]
+// var flags = false
+
+// for(var i = 0; i < cities.length; i++){
+//     if(cities[i] === "Faislabad"){
+//        flags = true
+//     }
+// }
+
+// if(flags){
+//     console.log("Mill gaya");
+// }else{
+//     console.log("Nahi mila");
+// }
+
+
+// ============== flag variable , boolean values and break =============
+// var cities = ["Karachi","Islamabad","Multan","Quetta","Lahore","Peshawar","Hyderabad"]
+// var flags = false
+
+// for(var i = 0; i < cities.length; i++){
+//     if(cities[i] === "Islamabad"){
+//        flags = true
+//        console.log("Mill gaya");
+//        break;
+//     }
+// }
+
+// if(!flags){
+//     console.log("Nahii mila");
+// }
+ 
+
+
+// ========== break example ==========
+// for(var i = 1; i <= 10; i++){
+//     console.log(i);
+//     if(i === 5){
+//         break;
+//     }
+    
+// }
+
+
+
+// Assignment swap the values
+// var students = ["Zoha","Rubina","Saba"]
