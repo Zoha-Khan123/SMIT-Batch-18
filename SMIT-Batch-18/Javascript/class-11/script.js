b9fd9828a34515297cf6d14dc01b8f844cbf090d
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-03-07 12:39:27 +0500

    class 11 code

---
+++ SMIT-Batch-18/Javascript/class-11/script.js
@@ -0,0 +1,59 @@
+// ============= Changing Case =================
+
+// var cities = ["karachi","islamabad","quetta"]
+
+// var user = prompt("Enter city name").toLowerCase()
+
+// for(var i = 0; i < cities.length; i++){
+
+//     if(cities[i] === user){
+//         console.log("found");
+        
+//     }
+    
+// }
+
+
+
+// =============== First letter capital ================
+// console.log(userName.slice(0,1).toUpperCase() + userName.slice(1));
+// var userName = prompt("Enter your name")
+
+
+
+// =============== Each word first letter capital ===========
+// var userName = prompt("Enter your name")
+// var userNameSplit = userName.split(" ")
+// // console.log(userNameSplit);
+// var arr = []
+// for(var i = 0; i < userNameSplit.length; i++){
+//     arr.push(userNameSplit[i].slice(0,1).toUpperCase()+userNameSplit[i].slice(1).toLowerCase());
+    
+// }
+// console.log(arr);
+// console.log(arr.join(" "));
+
+
+
+// ============ month first three letter extract =========
+// january
+// var month = prompt("Enter month name")
+// if(month.length >= 3){
+//     month = month.slice(0,3)
+//     console.log(month);
+    
+// }
+
+
+// ============= search word ================
+// var text = "You can use the character count as the loop limiter."
+// console.log(text.slice(0,5));
+
+
+// for(var i = 0; i < text.length; i++){
+//     console.log(text.slice(i,i+9));
+//     if(text.slice(i,i+9) === "character"){
+//         console.log("found");
+        
+//     }
+// }
