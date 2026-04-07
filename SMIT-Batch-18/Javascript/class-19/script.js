2865785912f355c59ac3cb29b2a27322ae5ea568
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-04-07 23:12:26 +0500

    class 19 code

---
+++ SMIT-Batch-18/Javascript/class-19/script.js
@@ -0,0 +1,47 @@
+// var rounds = Number(prompt("How many rounds do you want to play the game"))
+
+// var palyer1Score = 0;  
+// var palyer2Score = 0; 
+
+// for(var i = 1; i <= rounds; i++){
+    
+//     console.log("Round " + i);
+    
+//     var player1Roll = Math.floor((Math.random() * 6) + 1)
+//     console.log("Player 1 Roll " + player1Roll);  5
+    
+//     var player2Roll = Math.floor((Math.random() * 6) + 1)
+//     console.log("Player 2 Roll " + player2Roll);  5
+
+//     if(player1Roll > player2Roll){
+//         palyer1Score++
+//     }else if(player2Roll > player1Roll){
+//         palyer2Score++
+//     }else{
+//         console.log("Draw");
+        
+//     }
+    
+//     console.log("=======================");
+    
+// }
+
+// console.log("====== x ======== Result ====== x ========");
+
+// console.log("Player 1 score :" + palyer1Score);
+// console.log("Player 2 score :" + palyer2Score);
+
+
+// if(palyer1Score > palyer2Score){
+//     console.log("Player 1 won the match");
+    
+// }else if(palyer2Score > palyer1Score){
+//     console.log("Player 2 won the match");
+    
+// }else{
+//     console.log("Match draw");
+    
+// }
+
+
+
