c1c24ba815bcdb35f43a2d42ad27f84e53850793
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-04-26 01:12:37 +0500

    class 22 code

---
+++ SMIT-Batch-18/Javascript/class-22/script.js
@@ -0,0 +1,47 @@
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+
+// function greetings(){
+//     console.log("function Hello world");
+// }
+
+// greetings()
+// greetings()
+
+// function reverse(a,b){
+//     for(var i = a.length - 1; i >= 0; i--){
+//        console.log(a[i]);
+
+//     }
+//     console.log(b);
+// }
+
+// // var userName = prompt("Enter your name")
+// reverse("zoha",34)
+
+// function sum(num1, num2){
+//     console.log(num1 + num2);
+
+// }
+// sum(4,5)
+// sum(8,9)
+
+// function calculation(num1,num2,operator){
+
+//     if(operator === "+"){
+//         console.log(num1 + num2);
+
+//     }else if(operator === "-"){
+//         console.log(num1 + num2);
+//     }else if(operator === "*"){
+//          console.log(num1 * num2);
+//     }
+
+// }
+
+// var userNum1 = +prompt("Enter first number")
+// var userNum2 = +prompt("Enter second number")
+// var userOperator = prompt("Enter operator (+,-,*)")
+// calculation(userNum1,userNum2,userOperator)
