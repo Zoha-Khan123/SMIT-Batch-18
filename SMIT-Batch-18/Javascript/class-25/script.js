f17a41bdcfc7cdcfaca89aa3619ee570d30dfd38
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-05-08 11:31:35 +0500

    class 25 code loops

---
+++ SMIT-Batch-18/Javascript/class-25/script.js
@@ -0,0 +1,73 @@
+// =========== for loop ==========
+// for(var i = 1; i <= 10; i++){
+//     console.log(i);
+    
+// }
+
+
+// ======== while loop =============
+// var i = 0
+// while(i < 0){
+//     console.log(i);
+//     i++
+// }
+
+
+// ======== do while loop ==========
+// var i = 0
+// do{
+//     console.log(i);
+//     i++
+// }while(i < 0)
+
+
+// =========== for loop example =========
+// var password;
+// for(var i = 1; i < 5; i++){
+//     password = prompt("Enter your password")
+//     if(password === "12345"){
+//         console.log("correct password");
+//         break;
+//     }else{
+//         console.log("wrong password");
+        
+//     }
+// }
+
+// ========= while loop example ==============
+// var password;
+// while(password !== "12345"){
+//     password = prompt("Enter password")
+//     if(password !== "12345"){
+//         console.log("wrong password");
+        
+//     }
+// }
+// console.log("correct paassword");
+
+
+// ============ do while loop example ===========
+// var password;
+// do{
+//     password = prompt("Enter password")
+//     if(password !== "12345"){
+//         console.log("wrong password");
+        
+//     }
+// }while(password !== "12345")
+// console.log("correct password");
+    
+
+// ====== full line comment ========
+// var myName = "zoha"
+
+
+// =========== half line comment ========
+var myName = "Zoha" //name store in varaible
+
+
+// ====== multi line comment ============
+/* console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world"); */
