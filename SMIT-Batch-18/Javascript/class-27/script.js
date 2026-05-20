475bd5edf912f092f48f65ff6d0438d98bf888b1
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-05-20 11:37:42 +0500

    class 27 dom

---
+++ SMIT-Batch-18/Javascript/class-27/script.js
@@ -0,0 +1,7 @@
+
+// console.dir(window.document.documentElement.children[1].children[0])
+// console.log(window.document.documentElement.children[1].children[0])
+
+var haeding = document.getElementById("naheera")
+// haeding.textContent
+console.log(haeding.textContent = "hello world")
