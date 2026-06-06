25ab4aa2de955463b355a754b4fe7947d2d31103
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-06-06 21:43:43 +0500

    class 28 code  dom selection methods

---
+++ SMIT-Batch-18/Javascript/class-28/script.js
@@ -0,0 +1,38 @@
+// ======= Selection Methods ========
+// var ele = document.getElementById("heading1")
+// console.dir(ele)
+// console.log(ele);
+
+
+// var ele = document.getElementsByClassName("heading1")
+
+// for(var i = 0; i < ele.length; i++){
+//     console.log(ele[i])
+//     console.dir(ele[i])
+
+// }
+
+
+
+// var ele = document.getElementsByTagName("p")[1]
+// console.dir(ele)
+// console.log(ele);
+
+
+// var ele = document.querySelector("h1");
+// console.log(ele);
+// console.dir(ele)
+
+
+// var ele = document.querySelectorAll("#para");
+// var arr = Array.from(ele)
+// console.log(arr);
+// console.dir(arr)
+
+
+// =========== properties ==========
+var ele = document.getElementById("heading1")
+console.log(ele.innerText);
+console.log(ele.textContent);
+console.log(ele.innerHTML);
+
